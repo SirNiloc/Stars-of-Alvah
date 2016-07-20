8b834a8ece979beac523ba035935584da19c7ddc
Colin Korensky <sirniloc181965@gmail.com>
2016-07-20 02:40:49 -0500

    Stuff

---
+++ StarsOfAlvah/src/soa/item/material/raw/Gem.java
@@ -0,0 +1,10 @@
+package soa.item.material.raw;
+
+public class Gem extends MaterialRaw{
+
+	public Gem(String n, int t) {
+		super(n, t);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ StarsOfAlvah/src/soa/item/material/raw/Plant.java
@@ -0,0 +1,10 @@
+package soa.item.material.raw;
+
+public class Plant extends MaterialRaw{
+
+	public Plant(String n, int t) {
+		super(n, t);
+		// TODO Auto-generated constructor stub
+	}
+
+}
